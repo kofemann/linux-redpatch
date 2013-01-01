9cce04c60da1f49ca7ba5ef8b3382231a036f6d2
Jiri Pirko <jiri@resnulli.us>
2013-01-01 03:30:14 +0000

    net: call add_device_randomness() only after successful mac change
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Jamie Iles <jamie.iles@oracle.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5475,9 +5475,10 @@ int dev_set_mac_address(struct net_device *dev, struct sockaddr *sa)
 	if (!netif_device_present(dev))
 		return -ENODEV;
 	err = ops->ndo_set_mac_address(dev, sa);
-	if (!err)
-		call_netdevice_notifiers(NETDEV_CHANGEADDR, dev);
-	return err;
+	if (err)
+		return err;
+	call_netdevice_notifiers(NETDEV_CHANGEADDR, dev);
+	return 0;
 }
 EXPORT_SYMBOL(dev_set_mac_address);
 
