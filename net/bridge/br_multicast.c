ef6c14b9656c82f299079c448eca019ad6e7e111
Michael Ploujnikov <michael.ploujnikov@oracle.com>
2013-11-24 20:01:28 -0500

    [net] bridge: send query as soon as leave is received (Amerigo Wang) [997745 952012]
    Based on commit:
    
    commit 6b7df111ece130fa979a0c4f58e53674c1e47d3e
    Author: Cong Wang <amwang@redhat.com>
    Date:   Tue May 21 21:52:56 2013 +0000
    
        bridge: send query as soon as leave is received
    
        Continue sending queries when leave is received if the user marks
        it as a querier.
    
        Cc: Herbert Xu <herbert@gondor.apana.org.au>
        Cc: Stephen Hemminger <stephen@networkplumber.org>
        Cc: "David S. Miller" <davem@davemloft.net>
        Cc: Adam Baker <linux@baker-net.org.uk>
        Signed-off-by: Cong Wang <amwang@redhat.com>
        Acked-by: Herbert Xu <herbert@gondor.apana.org.au>
        Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_multicast.c
+++ net/bridge/br_multicast.c
@@ -1277,6 +1277,30 @@ static void br_multicast_leave_group(struct net_bridge *br,
 	if (!mp)
 		goto out;
 
+	if (br->multicast_querier &&
+	    !timer_pending(&br->multicast_querier_timer)) {
+		__br_multicast_send_query(br, port, &mp->addr);
+
+		time = jiffies + br->multicast_last_member_count *
+				 br->multicast_last_member_interval;
+		mod_timer(port ? &port->multicast_query_timer :
+				 &br->multicast_query_timer, time);
+
+		for (p = mp->ports; p; p = p->next) {
+			if (p->port != port)
+				continue;
+
+			if (!hlist_unhashed(&p->mglist) &&
+			    (timer_pending(&p->timer) ?
+			     time_after(p->timer.expires, time) :
+			     try_to_del_timer_sync(&p->timer) >= 0)) {
+				mod_timer(&p->timer, time);
+			}
+
+			break;
+		}
+	}
+
 	now = jiffies;
 	time = now + br->multicast_last_member_count *
 		     br->multicast_last_member_interval;
