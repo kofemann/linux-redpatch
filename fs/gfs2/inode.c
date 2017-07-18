6d4ab0a78b9e4765967164984b9d895d2dfad27f
Bob Peterson <rpeterso@redhat.com>
2017-07-18 12:15:01 -0500

    GFS2: Set gl_object in inode lookup only after block type check
    Before this patch, the inode glock's gl_object was set after a
    reference was acquired, but before the block type was verified.
    In cases where the block was unlinked, then freed and reused on
    another node, a residule delete callback (delete_work) would try
    to look up the inode, eventually failing the block check, but
    only after it overwrites gl_object with a pointer to the wrong
    inode. This patch moves the assignment of gl_object after the
    block check so it won't be improperly overwritten.
    
    Likewise, at the end of the function, gfs2_inode_lookup was
    clearing gl_object after it unlocked the glock, which meant
    another process might free the glock in the meantime. This
    patch guards against that case.
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Reviewed-by: Andreas Gruenbacher <agruenba@redhat.com>
    (cherry picked from commit 4d7c18c7df89ef549f2de79b0faf873b49dea57a)

--- fs/gfs2/inode.c
+++ fs/gfs2/inode.c
@@ -146,7 +146,6 @@ struct inode *gfs2_inode_lookup(struct super_block *sb, unsigned int type,
 		error = gfs2_glock_get(sdp, no_addr, &gfs2_inode_glops, CREATE, &ip->i_gl);
 		if (unlikely(error))
 			goto fail;
-		ip->i_gl->gl_object = ip;
 
 		error = gfs2_glock_get(sdp, no_addr, &gfs2_iopen_glops, CREATE, &io_gl);
 		if (unlikely(error))
@@ -172,13 +171,14 @@ struct inode *gfs2_inode_lookup(struct super_block *sb, unsigned int type,
 					goto fail_put;
 			}
 		}
+		glock_set_object(ip->i_gl, ip);
 
 		set_bit(GIF_INVALID, &ip->i_flags);
 		error = gfs2_glock_nq_init(io_gl, LM_ST_SHARED, GL_EXACT, &ip->i_iopen_gh);
 		if (unlikely(error))
 			goto fail_put;
 
-		ip->i_iopen_gh.gh_gl->gl_object = ip;
+		glock_set_object(io_gl, ip);
 		gfs2_glock_put(io_gl);
 		io_gl = NULL;
 
@@ -796,7 +796,7 @@ int gfs2_create_inode(struct inode *dir, struct dentry *dentry,
 	if (error)
 		goto fail_free_inode;
 
-	ip->i_gl->gl_object = ip;
+	glock_set_object(ip->i_gl, ip);
 	error = gfs2_glock_nq_init(ip->i_gl, LM_ST_EXCLUSIVE, GL_SKIP, ghs + 1);
 	if (error)
 		goto fail_free_inode;
@@ -818,7 +818,7 @@ int gfs2_create_inode(struct inode *dir, struct dentry *dentry,
 	if (error)
 		goto fail_gunlock2;
 
-	ip->i_iopen_gh.gh_gl->gl_object = ip;
+	glock_set_object(io_gl, ip);
 	gfs2_glock_put(io_gl);
 	gfs2_set_iop(inode);
 	insert_inode_hash(inode);
